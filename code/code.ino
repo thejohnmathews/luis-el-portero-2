d2d459bd02a0f2af89a8f7508b86bf25e5c60503
thejohnmathews <thejohnmathews@gmail.com>
2025-02-02 01:36:00 +0100

    works good now, adding features

--- code/code.ino
+++ code/code.ino
@@ -8,26 +8,16 @@
 #include <Servo.h>
 #include "environment.h"
 
-// global variables - networking
-const char* ssid = SSID;
-const char* password = PASSWORD;
-const char* serverURL = SERVERURL;
-const char* ssl = FINGERPRINT;
-
 // global variables - SG90
 Servo SG90;
 const int SG90_signal = D4;
 const int normal_angle = 0;
 const int trigger_angle = 180;
 
-// code initialization
-void setup(){
-  
-  // initialize Serial and SG90 signal pin & angle
-  Serial.begin(115200);
-  SG90.attach(SG90_signal);
-  SG90.write(normal_angle);
-  Serial.println("NodeMCU v3 powered on, initializing ...");
+// global variables
+WiFiClientSecure client;
+
+void connect_wifi(){
 
   // attempt WiFi connection
   Serial.println("connecting to Wi-Fi...");
@@ -42,71 +32,101 @@ void setup(){
   Serial.println("\n\nWi-Fi connected. Starting code execution.\n");
 }
 
+void connect_server(){
+
+  // check SHA1 fingerprint of certificate
+  client.setFingerprint(FINGERPRINT);
+
+  // connect using SSL
+  Serial.println("connecting to server...");
+  if (!client.connect("johnmathews.tech", 443)){
+
+    Serial.println("SSL connection failed");
+    return;
+  }
+
+  Serial.println("SSL connection success");
+}
+
+// code initialization
+void setup(){
+  
+  // initialize Serial and SG90 signal pin & angle
+  Serial.begin(115200);
+  SG90.attach(SG90_signal);
+  SG90.write(normal_angle);
+  Serial.println("NodeMCU v3 powered on, initializing ...");
+
+  // connect to WiFi
+  connect_wifi();
+
+  // connect to server
+  connect_server();
+}
+
 // code execution
 void loop(){
-  
-  if (WiFi.status() == WL_CONNECTED){
 
-    WiFiClientSecure client;
+  Serial.println("entering loop() execution");
 
-    // connect via SSL
-    if (!client.connect("johnmathews.tech", 443)){
+  // while connected to WiFi & server, get GET requests
+  while(WiFi.status() == WL_CONNECTED && client.connected()){
 
-      Serial.println("SSL connection failed");
-      return;
-    }
-    
-    // verify SSL cert
-    if (!client.setFingerprint(FINGERPRINT)){ 
+    Serial.println("Sending GET request using SSL...");
 
-      Serial.println("SSL certificate verification failed");
-      return;
-    }
-
-    Serial.println("SSL certificate verified!");
-    
-    // send GET request
-    client.print(String("GET /trigger.php HTTP/1.1\r\n") +
-                 "Host: johnmathews.tech\r\n" +
-                 "Connection: close\r\n\r\n");
+    // send GET request to look at trigger's status
+    client.print(String("GET /pages/trigger_status.txt HTTP/1.1\r\n") +
+                  "Host: johnmathews.tech\r\n" +
+                  "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64)\r\n" +
+                  "Referer: https://johnmathews.tech\r\n" +
+                  "Connection: keep-alive\r\n\r\n");
 
     // wait for response
-    long timeout = millis(); 
-    while (client.available() == 0) {
-      if (millis() - timeout > 5000) {
-        Serial.println(">>> Client Timeout !");
-        client.stop();
-        return;
-      }
+    long timeout = millis();
+    while (client.available() == 0){
+
+        if (millis() - timeout > 5000){ 
+
+          Serial.println(">>> server 5sec response timeout");
+          return;
+        }
     }
 
-    // gather server response
+    // read response
     String payload = "";
     while (client.available()) payload += char(client.read());
 
     Serial.println("Server response: " + payload);
 
-    // does the response contain the trigger?
-    if (payload.indexOf("\"trigger\":true") >= 0){
-
-      Serial.println("trigger received, activating servo...");
-      SG90.write(trigger_angle);  
-      delay(5000);                   
-      SG90.write(normal_angle);  
-      delay(5000);                   
+    // check for code 500 response
+    if (payload.indexOf("500 Internal Server Error") >= 0){
+      
+      Serial.println("Received 500 Internal Server Error, switching to backup server...");
+      client.stop();
+      break;  
     }
 
-    // close connection
-    client.stop();
-  } 
-  else{
+    // check if trigger is active
+    if (payload.indexOf("true") >= 0) {
+        Serial.println("Trigger received, activating servo...");
+        SG90.write(trigger_angle);
+        delay(5000);
+        SG90.write(normal_angle);
+    }
 
-    // reconnect to WiFi
-    Serial.println("Wi-Fi not connected, retrying...");
-    WiFi.begin(ssid, password);
+    // wait before polling again
     delay(5000);
-  }
+  } 
 
-  // poll every 5 seconds
-  delay(5000);  
+  // regain wifi and/or server connection if lost
+  if (WiFi.status() != WL_CONNECTED){
+
+    Serial.println("WiFi connection lost, reconnecting...");
+    connect_wifi();
+  }
+  if (!client.connected()){
+
+    Serial.println("server connection lost, reconnecting...");
+    connect_server();
+  }
 }
